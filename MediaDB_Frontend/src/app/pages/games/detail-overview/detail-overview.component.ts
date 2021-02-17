5e381ba8601619dcedd8176e7522de880d6d12ec
jaufmann <waldemar.jaufmann@bcmsolutions.de>
2021-02-17 22:36:21 +0100

    asdasdfsdfsdfsdf

--- MediaDB_Frontend/src/app/pages/games/detail-overview/detail-overview.component.ts
+++ MediaDB_Frontend/src/app/pages/games/detail-overview/detail-overview.component.ts
@@ -22,7 +22,8 @@ export class DetailOverviewComponent implements OnInit {
   ngOnInit() {
     this.id = this.activeroute.snapshot.params['id'];
     console.log('test nein ich bin waldemar Jaufmann llllllllllll aaaaa asdasdf wadfasdfsdfsdfsdf');
-    console.log('ich bin waldemar ls lsl slslls lsls d');
+    console.log('ich bin waldemar');
+    console.log('test nein ich bin waldemar Jaufmann llllllllllll aaaaa asdasdf wadfasdfsdfsdfsdf');
     console.log('test nein ich bin waldemar kaufmann der erste und der zweite und der dritte und vierte und fünfte');
     this.getGameDetails(this.id);
   }
