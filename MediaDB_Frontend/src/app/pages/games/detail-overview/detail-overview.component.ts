89ccc9ca36425b59a920edc48d73a5a812fdb827
jaufmann <waldemar.jaufmann@bcmsolutions.de>
2021-02-17 21:58:55 +0100

    tripple

--- MediaDB_Frontend/src/app/pages/games/detail-overview/detail-overview.component.ts
+++ MediaDB_Frontend/src/app/pages/games/detail-overview/detail-overview.component.ts
@@ -25,6 +25,8 @@ export class DetailOverviewComponent implements OnInit {
     console.log('test nein ich bin waldemar');
     console.log('test nein ich bin waldemar');
     console.log('test nein ich bin waldemar');
+    console.log('test nein ich bin waldemar');
+    console.log('test nein ich bin waldemar');
     console.log('ich bin waldemar');
     console.log('test nein ich bin waldemar kaufmann');
     this.getGameDetails(this.id);
