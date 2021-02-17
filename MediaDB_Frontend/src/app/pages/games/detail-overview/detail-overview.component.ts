f7f8726757fe25b2872527ad4c6ec72818d0cd0c
jaufmann <waldemar.jaufmann@bcmsolutions.de>
2021-02-17 22:15:29 +0100

    fünfte

--- MediaDB_Frontend/src/app/pages/games/detail-overview/detail-overview.component.ts
+++ MediaDB_Frontend/src/app/pages/games/detail-overview/detail-overview.component.ts
@@ -23,7 +23,7 @@ export class DetailOverviewComponent implements OnInit {
     this.id = this.activeroute.snapshot.params['id'];
     console.log('test nein ich bin waldemar Jaufmann');
     console.log('ich bin waldemar');
-    console.log('test nein ich bin waldemar kaufmann der erste und der zweite und der dritte');
+    console.log('test nein ich bin waldemar kaufmann der erste und der zweite und der dritte und vierte und fünfte');
     this.getGameDetails(this.id);
   }
 
